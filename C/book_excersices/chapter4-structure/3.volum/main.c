b0e7b6dbd7206bda9263eb845e2d50b8938aa7e9
esraa237 <esraafoda2000@gmail.com>
2024-11-04 15:12:35 +0200

    Add 3rd problem in structures

---
+++ C/book_excersices/chapter4-structure/3.volum/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Create a structure called Volume that uses three variables of type Distance (from the
+ENGLSTRC example) to model the volume of a room. Initialize a variable of type Volume
+to specific dimensions, then calculate the volume it represents, and print out the result.
+To calculate the volume, convert each dimension from a Distance variable to a variable
+of type float representing feet and fractions of a foot, and then multiply the resulting
+three numbers.*/
+struct volume{
+
+};
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
