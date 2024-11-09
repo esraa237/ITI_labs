43394a6628a265b7608eda903434102bdba6d661
esraa237 <esraafoda2000@gmail.com>
2024-11-09 12:21:23 +0200

    Finish task2 in day1 Cpp OOP

---
+++ CPP/Day1-OOP/class-stack/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+class emp{
+int id;
+char name[10];
+int age;
+public:
+    void setId(int _id){
+        id=_id;
+    }
+    int getId(){
+        return id;
+    }
+    void setAge(int _age){
+        age=_age;
+    }
+    int getAge(){
+        return age;
+    }
+    void setName(char _name[]){
+        strcpy(name,_name);
+    }
+    char* getName(){
+        return name;
+    }
+};
+int main()
+{
+    int id,age;
+    char name[10];
+    emp e1;
+    emp* ptr=&e1;
+    cout<<"Enter your Id,Name,Age: "<<endl;
+    cin>>id>>name>>age;
+    ptr->setId(id);
+    ptr->setName(name);
+    ptr->setAge(age);
+    cout<<"ID: "<<ptr->getId()<<endl<<"Name: "<<ptr->getName()<<endl<<"Age: "<<ptr->getAge();
+    return 0;
+}
