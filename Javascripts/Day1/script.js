d5bf9e3fc82ba3380d00d57b6ad0a83b3ed13069
esraa237 <esraafoda2000@gmail.com>
2025-01-10 22:12:45 +0200

    add js file lab1 in js course

---
+++ Javascripts/Day1/script.js
@@ -0,0 +1,74 @@
+console.log("This is External Js File");
+//console.log("number1 before defenition: ",number2);//print undefined
+var number1=3;
+let number2=2.9;
+const number3= 0xff;
+
+var firstName="Esraa";
+console.log("FirstName before:",firstName);
+var middleName='Tarek';
+var lastName=`Foda`;
+var flag=true;
+firstName[3]="A";//ignore
+console.log("FirstName After:",firstName);
+
+
+var num=2;
+console.log(num);
+if(num%2==0)console.log("even");
+else console.log("odd");
+
+
+for(var i=0;i<=10;i++){
+    console.log(i);
+}
+
+
+// if(num==0)console.log("Zero");
+// else if(num>0)console.log("+ve");
+// else console.log("-ve");
+
+// for(var i=1;i<=num;i++){
+//     console.log(i+"*"+num+" "+i*num);
+// }
+// var day=3;
+// switch(day){
+//     case 7:
+//         console.log("Sat");
+//         console.log("Weekend");
+//         break;
+//     case 1:
+//         console.log("Sun");
+//         console.log("weekday");
+//         break;
+//     case 2:
+//         console.log("Mon");
+//         console.log("weekday");
+//         break;
+//     case 3:
+//         console.log("Tues");
+//         console.log("weekday");
+//         break;
+//     case 4:
+//         console.log("Wednes");
+//         console.log("weekday");
+//         break;
+//     case 5:
+//         console.log("Thurs");
+//         console.log("weekday");
+//         break;
+//     case 6:
+//         console.log("friday");
+//         console.log("Weekend");
+//         break;
+// }
+
+// // console.log("FirstName After:",firstName);
+// console.log("Type of");
+// console.log("number1: ",typeof number1);
+// console.log("number2: ",typeof number2);
+// console.log("number3: ",typeof number3);
+// console.log("firstName: ",typeof firstName);
+// console.log("lastName: ",typeof lastName);
+// console.log("middleName: ",typeof middleName);
+
